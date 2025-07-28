15ce0e44db98ab63494c28d1e8535c4b889ca36c
Yazawa-ctrl <84038454+Yazawa-ctrl@users.noreply.github.com>
2025-07-28 16:17:36 +0200

    Update vulnerable.js

--- vulnerable.js
+++ vulnerable.js
@@ -1,4 +1,4 @@
 const params = new URLSearchParams(window.location.search);
 const q = params.get("q")
 // XSS Vulnerability
-document.getElementById("output").innerHtml = q;
+$.(document.getElementById("output").innerHTML = q;
